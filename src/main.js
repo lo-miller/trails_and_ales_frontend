83d3febf6b1b6bb9d7af33b3dd10ea4c0565c403
Lauren Miller <lomiller0913@gmail>
2021-04-22 11:51:16 -0400

    initial commit, replicating gmap_test_vue

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from "axios"
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
+    libraries: 'places',
+  },
+  // installComponents: true
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <br>
+    <button v-on:click="hikesIndex">Load Map</button>
+    <br>
+    <div class="map">
+      <gmap-map
+      :center="center"
+      :zoom="zoom"
+      style="width:100%;  height: 400px;"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m"
+        @click="center=m"
+      >
+      </gmap-marker>
+    </gmap-map>
+    </div>
+  </div>
+</template> 
+
+<style>
+#map {
+  width: 100%;
+  height: 400px;
+}
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      center: { lat: 45.508, lng: -73.587 },
+      markers: [
+        { lat: 45.508, lng: -73.587 },
+        { lat: 44.508, lng: -73.587 },
+      ],
+      currentPlace: null,
+      zoom: 8,
+      hikes: [],
+    };
+  },
+  created: function () {},
+  methods: {
+    hikesIndex: function () {
+      console.log("fetching all hikes");
+      axios.get("/api/hikes").then((response) => {
+        console.log(response.data);
+        this.hikes = response.data;
+      });
+    },
+  },
+};
+</script>
+
